d0ca9563978cfa2124120fc5ab052103ded21db2
alfchee <alfchee@gmail.com>
2021-08-28 14:41:47 -0600

    using menu for multiple ubuntu versions

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,15 +1,68 @@
 #!/bin/bash
 
-sudo apt update
+##
+# Color  Variables
+##
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
 
-sudo apt install software-properties-common
+##
+# Color Functions
+##
 
-sudo apt-add-repository --yes --update ppa:ansible/ansible
+ColorGreen(){
+	echo -ne $green$1$clear
+}
+ColorBlue(){
+	echo -ne $blue$1$clear
+}
 
-sudo apt install ansible -y
+function install_ansible() {
+    echo ""
+    echo -ne "$(ColorGreen 'Installing Ansible')"
+    sudo apt update
 
-ansible-playbook setup.yml
+    sudo apt install software-properties-common
 
-echo '------------------------------------------'
-echo '  Please close and open terminal sesion'
-echo '------------------------------------------'
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+
+    sudo apt install ansible -y
+
+    echo ""
+}
+
+function run_playbook() {
+    echo ""
+    echo -ne "$(ColorGreen 'Running Ansible Playbook')"
+    echo ""
+
+    case $1 in 
+        1) ansible-playbook setup_18.04.yml ;;
+        2) ansible-playbook setup_20.04.yml ;;
+    esac
+
+    echo -ne "$(ColorBlue '------------------------------------------')"
+    echo -ne "$(ColorBlue '  Please close and open terminal sesion')"
+    echo -ne "$(ColorBlue '------------------------------------------')"
+}
+
+function menu() {
+    echo -ne "
+    Setting up CLI Tools
+
+    $(ColorGreen '1)') Ubuntu 18.04
+    $(ColorGreen '2)') Ubuntu 20.04
+    $(ColorGreen '0)') Exit
+    $(ColorBlue 'Choose an option:') "
+    read a
+    case $a in 
+        1) install_ansible; run_playbook $a; menu ;;
+        2) install_ansible; run_playbook $a; menu;;
+        0) exit 0;;
+        *) echo -ne $red"Wrong option.\n"$clear; exit 0 ;;
+    esac
+}
+
+## Calling the menu
+menu
